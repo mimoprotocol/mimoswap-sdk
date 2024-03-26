3687b774cba14cf53376fcdfe0548d9a0ede6cc8
osdodo <osdodo@qq.com>
2024-03-26 16:30:43 +0800

    first commit

---
+++ test/trade-iotex.test.ts
@@ -0,0 +1,71 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { parseUnits } from 'viem';
+import {
+  ChainId,
+  Token,
+  CurrencyAmount,
+  TradeType,
+  Percent,
+} from '../src/sdk-core';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import {
+  AlphaRouter,
+  ID_TO_CHAIN_ID,
+  ID_TO_PROVIDER,
+  SwapType,
+  TO_PROTOCOL,
+} from '../src';
+import { Protocol } from '@uniswap/router-sdk';
+import _ from 'lodash';
+
+describe('IOTEX Trade Test', () => {
+  const chainId = ID_TO_CHAIN_ID(ChainId.IOTEX);
+  const chainProvider = ID_TO_PROVIDER(chainId);
+  const provider = new JsonRpcProvider(chainProvider, chainId);
+  const protocolsStr = 'v2,v3';
+  let protocols: Protocol[] = [];
+  if (protocolsStr) {
+    try {
+      protocols = _.map(protocolsStr.split(','), (protocolStr) =>
+        TO_PROTOCOL(protocolStr)
+      );
+    } catch (err) {
+      throw new Error(
+        `Protocols invalid. Valid options: ${Object.values(Protocol)}`
+      );
+    }
+  }
+
+  it(`trade-iotex-test`, async () => {
+    const t1 = new Token(
+      chainId,
+      '0x6fbcdc1169b5130c59e72e51ed68a84841c98cd1',
+      18
+    );
+    const t2 = new Token(
+      chainId,
+      '0xa00744882684c3e4747faefd68d283ea44099d03',
+      18
+    );
+    const router = new AlphaRouter({
+      chainId,
+      provider,
+    });
+    const route = await router.route(
+      CurrencyAmount.fromRawAmount(t1, parseUnits('1', 18).toString()),
+      t2,
+      TradeType.EXACT_INPUT,
+      {
+        recipient: '0x0000000000000000000000000000000000000000',
+        slippageTolerance: new Percent(50, 10_000),
+        deadline: Math.floor(Date.now() / 1000 + 1800),
+        type: SwapType.SWAP_ROUTER_02,
+      },
+      {
+        protocols,
+      }
+    );
+    console.log('route=>', route);
+  });
+});
---
+++ test/trade.test.ts
@@ -0,0 +1,73 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { parseUnits } from 'viem';
+import {
+  ChainId,
+  Token,
+  CurrencyAmount,
+  TradeType,
+  Percent,
+} from '../src/sdk-core';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import {
+  AlphaRouter,
+  ID_TO_CHAIN_ID,
+  ID_TO_PROVIDER,
+  SwapType,
+  TO_PROTOCOL,
+} from '../src';
+import { Protocol } from '@uniswap/router-sdk';
+import _ from 'lodash';
+
+describe('MAINNET Trade Test', () => {
+  const chainId = ID_TO_CHAIN_ID(ChainId.MAINNET);
+  const chainProvider = ID_TO_PROVIDER(chainId);
+  const provider = new JsonRpcProvider(chainProvider, chainId);
+  const protocolsStr = 'v2,v3';
+  let protocols: Protocol[] = [];
+  if (protocolsStr) {
+    try {
+      protocols = _.map(protocolsStr.split(','), (protocolStr) =>
+        TO_PROTOCOL(protocolStr)
+      );
+    } catch (err) {
+      throw new Error(
+        `Protocols invalid. Valid options: ${Object.values(Protocol)}`
+      );
+    }
+  }
+
+  it(`trade-test`, async () => {
+    const wbtc = new Token(
+      chainId,
+      '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+      8
+    );
+    const weth = new Token(
+      chainId,
+      '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      18
+    );
+    const router = new AlphaRouter({
+      chainId,
+      provider,
+    });
+    const route = await router.route(
+      CurrencyAmount.fromRawAmount(wbtc, parseUnits('1', 8).toString()),
+      weth,
+      TradeType.EXACT_INPUT,
+      {
+        recipient: '0x0000000000000000000000000000000000000000',
+        slippageTolerance: new Percent(50, 10_000),
+
+        deadline: Math.floor(Date.now() / 1000 + 1800),
+
+        type: SwapType.SWAP_ROUTER_02,
+      },
+      {
+        protocols,
+      }
+    );
+    console.log('route=>', route);
+  });
+});
